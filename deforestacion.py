c2739b4d9aeb302ebd4916f931ef9471d0a788cd
jeferson-rpo <98125058+jeferson-rpo@users.noreply.github.com>
2025-02-03 16:02:08 -0500

    Update deforestacion.py

--- deforestacion.py
+++ deforestacion.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import geopandas as gpd
-import matplotlib.pyplot as plt
 
 # Título de la app
 st.title("Análisis de la Deforestación")
@@ -10,14 +8,14 @@ st.markdown("Este análisis permite visualizar las áreas deforestadas en el Ama
 # Subir el archivo CSV
 archivo = st.file_uploader("Sube un archivo CSV", type=["csv"])
 
-# Cargar el archivo CSV directamente al DataFrame
-gdf = pd.read_csv(archivo)  # No hay necesidad de if
+# Intentar cargar el archivo y asignar un DataFrame vacío si no se ha cargado un archivo
+try:
+    gdf = pd.read_csv(archivo)
+except:
+    gdf = pd.DataFrame()
 
 # Realizar la interpolación en las columnas de interés
 gdf[['Latitud', 'Longitud', 'Altitud', 'Precipitacion']] = gdf[['Latitud', 'Longitud', 'Altitud', 'Precipitacion']].interpolate()
 
 # Mostrar los primeros registros para verificar la carga de datos
 st.write(gdf.head())
-
-
-
