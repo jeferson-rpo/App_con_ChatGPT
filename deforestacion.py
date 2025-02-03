1925c6afc6c362b4d72656488690f8e146736bea
jeferson-rpo <98125058+jeferson-rpo@users.noreply.github.com>
2025-02-03 16:45:39 -0500

    Update deforestacion.py

--- deforestacion.py
+++ deforestacion.py
@@ -30,11 +30,7 @@ if 'gdf' in locals():  # Verificar si se cargaron datos
         gdf[gdf_numéricas.columns] = gdf_numéricas.fillna(gdf_numéricas.mean())  # Rellenar NaN con la media
 
     # Limpiar las columnas de fechas
-    gdf_fechas = gdf.select_dtypes(include=['object'])
-    gdf_fechas = gdf_fechas.apply(pd.to_datetime, errors='coerce')  # Convertir a datetime, forzando errores a NaT
-    if not gdf_fechas.empty:
-        fecha_promedio = gdf_fechas.mean()  # Calcular el promedio de las fechas
-        gdf[gdf_fechas.columns] = gdf_fechas.fillna(fecha_promedio)  # Rellenar con el promedio
+    
 
     # Limpiar las columnas de texto (tipo 'object' o 'string') con el valor más frecuente
     gdf_texto = gdf.select_dtypes(include=['object', 'string'])
